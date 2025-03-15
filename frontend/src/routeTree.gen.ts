b8e0bf94771fd01b6ad07966241e2a8f8501a68c
kamzhanyue <kamzhanyue@gmail.com>
2025-03-16 00:41:03 +1100

    chore: remove old routes

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -16,10 +16,6 @@ import { Route as LoginImport } from './routes/login'
 import { Route as GameImport } from './routes/game'
 import { Route as AboutImport } from './routes/about'
 import { Route as IndexImport } from './routes/index'
-import { Route as GameIndexImport } from './routes/game/index'
-import { Route as GameSendImport } from './routes/game/send'
-import { Route as GameReadImport } from './routes/game/read'
-import { Route as GameFishingImport } from './routes/game/fishing'
 
 // Create/Update Routes
 
@@ -51,30 +47,6 @@ const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRoute,
-} as any)
-
-const GameIndexRoute = GameIndexImport.update({
-  id: '/',
-  path: '/',
-  getParentRoute: () => GameRoute,
-} as any)
-
-const GameSendRoute = GameSendImport.update({
-  id: '/send',
-  path: '/send',
-  getParentRoute: () => GameRoute,
-} as any)
-
-const GameReadRoute = GameReadImport.update({
-  id: '/read',
-  path: '/read',
-  getParentRoute: () => GameRoute,
-} as any)
-
-const GameFishingRoute = GameFishingImport.update({
-  id: '/fishing',
-  path: '/fishing',
-  getParentRoute: () => GameRoute,
 } as any)
 
 // Populate the FileRoutesByPath interface
@@ -116,131 +88,49 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof SignupImport
       parentRoute: typeof rootRoute
     }
-    '/game/fishing': {
-      id: '/game/fishing'
-      path: '/fishing'
-      fullPath: '/game/fishing'
-      preLoaderRoute: typeof GameFishingImport
-      parentRoute: typeof GameImport
-    }
-    '/game/read': {
-      id: '/game/read'
-      path: '/read'
-      fullPath: '/game/read'
-      preLoaderRoute: typeof GameReadImport
-      parentRoute: typeof GameImport
-    }
-    '/game/send': {
-      id: '/game/send'
-      path: '/send'
-      fullPath: '/game/send'
-      preLoaderRoute: typeof GameSendImport
-      parentRoute: typeof GameImport
-    }
-    '/game/': {
-      id: '/game/'
-      path: '/'
-      fullPath: '/game/'
-      preLoaderRoute: typeof GameIndexImport
-      parentRoute: typeof GameImport
-    }
   }
 }
 
 // Create and export the route tree
 
-interface GameRouteChildren {
-  GameFishingRoute: typeof GameFishingRoute
-  GameReadRoute: typeof GameReadRoute
-  GameSendRoute: typeof GameSendRoute
-  GameIndexRoute: typeof GameIndexRoute
-}
-
-const GameRouteChildren: GameRouteChildren = {
-  GameFishingRoute: GameFishingRoute,
-  GameReadRoute: GameReadRoute,
-  GameSendRoute: GameSendRoute,
-  GameIndexRoute: GameIndexRoute,
-}
-
-const GameRouteWithChildren = GameRoute._addFileChildren(GameRouteChildren)
-
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
-  '/game': typeof GameRouteWithChildren
+  '/game': typeof GameRoute
   '/login': typeof LoginRoute
   '/signup': typeof SignupRoute
-  '/game/fishing': typeof GameFishingRoute
-  '/game/read': typeof GameReadRoute
-  '/game/send': typeof GameSendRoute
-  '/game/': typeof GameIndexRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/game': typeof GameRoute
   '/login': typeof LoginRoute
   '/signup': typeof SignupRoute
-  '/game/fishing': typeof GameFishingRoute
-  '/game/read': typeof GameReadRoute
-  '/game/send': typeof GameSendRoute
-  '/game': typeof GameIndexRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
-  '/game': typeof GameRouteWithChildren
+  '/game': typeof GameRoute
   '/login': typeof LoginRoute
   '/signup': typeof SignupRoute
-  '/game/fishing': typeof GameFishingRoute
-  '/game/read': typeof GameReadRoute
-  '/game/send': typeof GameSendRoute
-  '/game/': typeof GameIndexRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths:
-    | '/'
-    | '/about'
-    | '/game'
-    | '/login'
-    | '/signup'
-    | '/game/fishing'
-    | '/game/read'
-    | '/game/send'
-    | '/game/'
+  fullPaths: '/' | '/about' | '/game' | '/login' | '/signup'
   fileRoutesByTo: FileRoutesByTo
-  to:
-    | '/'
-    | '/about'
-    | '/login'
-    | '/signup'
-    | '/game/fishing'
-    | '/game/read'
-    | '/game/send'
-    | '/game'
-  id:
-    | '__root__'
-    | '/'
-    | '/about'
-    | '/game'
-    | '/login'
-    | '/signup'
-    | '/game/fishing'
-    | '/game/read'
-    | '/game/send'
-    | '/game/'
+  to: '/' | '/about' | '/game' | '/login' | '/signup'
+  id: '__root__' | '/' | '/about' | '/game' | '/login' | '/signup'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   AboutRoute: typeof AboutRoute
-  GameRoute: typeof GameRouteWithChildren
+  GameRoute: typeof GameRoute
   LoginRoute: typeof LoginRoute
   SignupRoute: typeof SignupRoute
 }
@@ -248,7 +138,7 @@ export interface RootRouteChildren {
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   AboutRoute: AboutRoute,
-  GameRoute: GameRouteWithChildren,
+  GameRoute: GameRoute,
   LoginRoute: LoginRoute,
   SignupRoute: SignupRoute,
 }
@@ -277,35 +167,13 @@ export const routeTree = rootRoute
       "filePath": "about.tsx"
     },
     "/game": {
-      "filePath": "game.tsx",
-      "children": [
-        "/game/fishing",
-        "/game/read",
-        "/game/send",
-        "/game/"
-      ]
+      "filePath": "game.tsx"
     },
     "/login": {
       "filePath": "login.tsx"
     },
     "/signup": {
       "filePath": "signup.tsx"
-    },
-    "/game/fishing": {
-      "filePath": "game/fishing.tsx",
-      "parent": "/game"
-    },
-    "/game/read": {
-      "filePath": "game/read.tsx",
-      "parent": "/game"
-    },
-    "/game/send": {
-      "filePath": "game/send.tsx",
-      "parent": "/game"
-    },
-    "/game/": {
-      "filePath": "game/index.tsx",
-      "parent": "/game"
     }
   }
 }
--- frontend/src/routes/game/fishing.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/game/fishing")({
-  component: Fishing,
-});
-
-function Fishing() {
-  return <div>Hello "/game/fishing"!</div>;
-}
