1f1c519f48280d76a38153ae0688263198c95c3b
danielhong <dan.hong378@gmail.com>
2025-03-15 12:10:12 +1100

    feat: add models

--- backend/bottle_backend/bottle/models.py
+++ backend/bottle_backend/bottle/models.py
@@ -1,7 +1,24 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+
 class Bottle(models.Model):
-    id=models.IntegerField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text=models.TextField()
-    sender=models.IntegerField()
+    sender=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bottle = models.ForeignKey(Bottle, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
