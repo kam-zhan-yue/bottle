e0abcbd9b0e6c658835510824b1315168dca5edc
kamzhanyue <kamzhanyue@gmail.com>
2025-03-15 22:15:48 +1100

    fix: fix space and arrow keys eaten up

--- frontend/src/game/classes/InputHandler.ts
+++ frontend/src/game/classes/InputHandler.ts
@@ -1,49 +1,60 @@
 export default class InputHandler {
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private wasd!: {
+  private controls!: {
+    upArrow: Phaser.Input.Keyboard.Key;
+    downArrow: Phaser.Input.Keyboard.Key;
+    leftArrow: Phaser.Input.Keyboard.Key;
+    rightArrow: Phaser.Input.Keyboard.Key;
     up: Phaser.Input.Keyboard.Key;
     down: Phaser.Input.Keyboard.Key;
     left: Phaser.Input.Keyboard.Key;
     right: Phaser.Input.Keyboard.Key;
+    space: Phaser.Input.Keyboard.Key;
   };
 
   constructor(scene: Phaser.Scene) {
-    this.cursors = scene.input.keyboard
-      ? scene.input.keyboard.createCursorKeys()
-      : ({} as Phaser.Types.Input.Keyboard.CursorKeys);
-    this.wasd = scene.input.keyboard?.addKeys(
+    this.controls = scene.input.keyboard?.addKeys(
       {
+        upArrow: Phaser.Input.Keyboard.KeyCodes.UP,
+        downArrow: Phaser.Input.Keyboard.KeyCodes.DOWN,
+        leftArrow: Phaser.Input.Keyboard.KeyCodes.LEFT,
+        rightArrow: Phaser.Input.Keyboard.KeyCodes.RIGHT,
         up: Phaser.Input.Keyboard.KeyCodes.W,
         down: Phaser.Input.Keyboard.KeyCodes.S,
         left: Phaser.Input.Keyboard.KeyCodes.A,
         right: Phaser.Input.Keyboard.KeyCodes.D,
+        space: Phaser.Input.Keyboard.KeyCodes.SPACE,
       },
       false,
     ) as {
+      upArrow: Phaser.Input.Keyboard.Key;
+      downArrow: Phaser.Input.Keyboard.Key;
+      leftArrow: Phaser.Input.Keyboard.Key;
+      rightArrow: Phaser.Input.Keyboard.Key;
       up: Phaser.Input.Keyboard.Key;
       down: Phaser.Input.Keyboard.Key;
       left: Phaser.Input.Keyboard.Key;
       right: Phaser.Input.Keyboard.Key;
+      space: Phaser.Input.Keyboard.Key;
     };
   }
 
   public isDown(): boolean {
-    return this.cursors?.down?.isDown || this.wasd?.down?.isDown;
+    return this.controls?.downArrow?.isDown || this.controls?.down?.isDown;
   }
 
   public isUp(): boolean {
-    return this.cursors?.up?.isDown || this.wasd?.up?.isDown;
+    return this.controls?.upArrow?.isDown || this.controls?.up?.isDown;
   }
 
   public isRight(): boolean {
-    return this.cursors?.right?.isDown || this.wasd?.right?.isDown;
+    return this.controls?.rightArrow?.isDown || this.controls?.right?.isDown;
   }
 
   public isLeft(): boolean {
-    return this.cursors?.left?.isDown || this.wasd?.left?.isDown;
+    return this.controls?.leftArrow?.isDown || this.controls?.left?.isDown;
   }
 
   public isInteractDown(): boolean {
-    return Phaser.Input.Keyboard.JustDown(this.cursors?.space);
+    return Phaser.Input.Keyboard.JustDown(this.controls?.space);
   }
 }
