9b896d6c3187e3d7cb0594bed5f2f6df54a97c2d
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-03-15 16:00:56 +1100

    Added user and message to admin site

--- backend/bottle_backend/bottle/admin.py
+++ backend/bottle_backend/bottle/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Bottle
+from .models import Bottle, User, Message
 
 admin.site.register(Bottle)
+admin.site.register(User)
+admin.site.register(Message)
