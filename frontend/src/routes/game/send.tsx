ec9d418d3ea85080e6c9f2e3c5aa3ba0a78333bd
kamzhanyue <kamzhanyue@gmail.com>
2025-03-15 23:10:17 +1100

    wip: wip

--- frontend/src/routes/game/send.tsx
+++ frontend/src/routes/game/send.tsx
@@ -4,6 +4,12 @@ import Overlay from "../../components/Overlay";
 import scroll from "../../assets/scroll.png";
 import sendButtonDefault from "../../assets/send_button_default.png";
 import sendButtonHover from "../../assets/send_button_hover.png";
+import {
+  WebSocketContext,
+  WebSocketContextType,
+} from "../../contexts/WebSocketContext";
+import { useContext, useEffect, useState } from "react";
+import { MessageAction } from "../../utils/Interaction";
 
 export const Route = createFileRoute("/game/send")({
   component: Send,
@@ -11,51 +17,74 @@ export const Route = createFileRoute("/game/send")({
 
 function Send() {
   const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+
+  const { sendJsonMessage, lastJsonMessage } = useContext(
+    WebSocketContext,
+  ) as WebSocketContextType;
+
+  useEffect(() => {
+    console.log("Last JSON Message is ", lastJsonMessage);
+  }, [lastJsonMessage]);
+
+  const onSubmit = (message: string) => {
+    console.log("Sending ", message);
+  };
   return (
     <Overlay>
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="absolute w-1/2 h-auto max-w-xl">
-          <img
-              src={scroll}
-              alt="Scroll"
-              className="scroll"
-            />
-        </div>
-        <div className="absolute w-1/2 h-auto max-w-xl p-15">
-          <p className="text-xl text-center leading-tight mb-4" style={{ fontFamily: 'PixelifySans', color: '#875A3A' }}>
-            Send a Message:
-          </p>
-          <textarea
-            className="bg-transparent text-black placeholder-black text-lg w-4/4 p-2 border-none outline-none resize-none h-50 overflow-auto"
-            placeholder="Type here..."
-            style={{
-              fontFamily: "PixelifySans",
-              whiteSpace: "pre-wrap",
-            }}
-          />
-          <div className="w-full flex justify-center items-center mt-6">
-            <button
-              className="py-4 px-8 w-40 h-14 bg-no-repeat bg-contain border-none outline-none focus:outline-none active:outline-none"
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(message);
+        }}
+      >
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="absolute w-1/2 h-auto max-w-xl">
+            <img src={scroll} alt="Scroll" className="scroll" />
+          </div>
+          <div className="absolute w-1/2 h-auto max-w-xl p-15">
+            <p
+              className="text-xl text-center leading-tight mb-4"
+              style={{ fontFamily: "PixelifySans", color: "#875A3A" }}
+            >
+              Send a Message:
+            </p>
+            <textarea
+              className="bg-transparent text-black placeholder-black text-lg w-4/4 p-2 border-none outline-none resize-none h-50 overflow-auto"
+              placeholder="Type here..."
               style={{
-                backgroundImage: `url(${sendButtonDefault})`,
-                backgroundSize: "contain",
-                backgroundColor: "transparent",
                 fontFamily: "PixelifySans",
-                textAlign: "center",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                border: "none", 
-                outline: "none", 
-                boxShadow: "none",
+                whiteSpace: "pre-wrap",
               }}
-              onMouseEnter={(e) => (e.currentTarget.style.backgroundImage = `url(${sendButtonHover})`)}
-              onMouseLeave={(e) => (e.currentTarget.style.backgroundImage = `url(${sendButtonDefault})`)}
-              onClick={() => navigate({ to: "/game" })}
             />
+            <div className="w-full flex justify-center items-center mt-6">
+              <button
+                className="py-4 px-8 w-40 h-14 bg-no-repeat bg-contain border-none outline-none focus:outline-none active:outline-none"
+                style={{
+                  backgroundImage: `url(${sendButtonDefault})`,
+                  backgroundSize: "contain",
+                  backgroundColor: "transparent",
+                  fontFamily: "PixelifySans",
+                  textAlign: "center",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  border: "none",
+                  outline: "none",
+                  boxShadow: "none",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.backgroundImage = `url(${sendButtonHover})`)
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.backgroundImage = `url(${sendButtonDefault})`)
+                }
+                onClick={() => navigate({ to: "/game" })}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </Overlay>
   );
 }
