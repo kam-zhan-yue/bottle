88c33ba2bbe5fb9913f47c563d54a2f819c24466
suchitrawat <rawats@student.unimelb.edu.au>
2025-03-15 19:05:57 +1100

    added sockets

--- backend/bottle_backend/bottle_backend/urls.py
+++ backend/bottle_backend/bottle_backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
+    path("", include("bottle.urls")),
 ]
