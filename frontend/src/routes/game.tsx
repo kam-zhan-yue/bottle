1b5537106ebae66e136d732260fbcd1b47b1f7d0
danielhong <dan.hong378@gmail.com>
2025-03-15 22:58:41 +1100

    update

--- frontend/src/routes/game.tsx
+++ frontend/src/routes/game.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { GameContext, GameContextType } from "../game/GameContext";
 import { EventBus } from "../EventBus";
 import Overlay from "../components/Overlay";
-import useWebSocket from "react-use-websocket";
 
 export const Route = createFileRoute("/game")({
   component: Game,
@@ -13,35 +12,11 @@ function Game() {
   const { island, user } = useContext(GameContext) as GameContextType;
   const navigate = useNavigate();
 
-  const WS_URL = "ws://localhost:8000/ws/chat/testroom/";
-  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
-    share: true,
-  });
-
-  if (lastJsonMessage) {
-    console.log("Last Json Message is ", lastJsonMessage);
-  }
-
   useEffect(() => {
     if (island) {
       island.initPlayer();
     }
   }, [island]);
-
-  useEffect(() => {
-    console.log("Send player");
-    sendJsonMessage({
-      message: "player is connected",
-    });
-  }, [sendJsonMessage]);
-
-  useEffect(() => {
-    if (!user) return;
-    sendJsonMessage({
-      message: "text",
-    });
-    console.log("Sending text");
-  }, [sendJsonMessage, user]);
 
   useEffect(() => {
     console.log("Initialising EventBus listeners");
@@ -60,7 +35,6 @@ function Game() {
       EventBus.off("mailbox");
       EventBus.off("note");
     };
-
   }, []);
 
   return (
