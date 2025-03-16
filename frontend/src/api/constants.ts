fc290b27ca50b0f4837012bbb250ddea3edf4a3c
danielhong <dan.hong378@gmail.com>
2025-03-16 16:29:00 +1100

    depl

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,3 +1,3 @@
 
 // const BASE_URL = process.env.BASE_URL;
-export const BASE_URL = 'http://bottle-backend.eba-vbbwx49g.ap-southeast-2.elasticbeanstalk.com';
+export const BASE_URL = 'https://bottle-backend.eba-vbbwx49g.ap-southeast-2.elasticbeanstalk.com';
